8b9fa219d438aa76923b5135b4f0e789746eac5c
Justin Latimer <justinlatimer@gmail.com>
2015-10-27 07:53:08 +0000

    Add an index for webpack.

---
+++ index.js
@@ -0,0 +1,10 @@
+require('./src/CognitoSyncManager');
+require('./src/CognitoSyncConflict');
+require('./src/CognitoSyncDataset');
+require('./src/CognitoSyncDatasetMetadata');
+require('./src/CognitoSyncDatasetUpdates');
+require('./src/CognitoSyncLocalStorage');
+require('./src/CognitoSyncRecord');
+require('./src/CognitoSyncRemoteStorage');
+require('./src/CognitoSyncStoreInMemory');
+require('./src/CognitoSyncStoreLocalStorage');
