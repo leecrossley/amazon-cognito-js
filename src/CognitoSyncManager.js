a9f5c53a61f8d7ab13dc8db1356ddfd0d8b072fd
mgoria <goria.marcel@gmail.com>
2016-01-05 19:33:31 +0200

    Check if localStorage is available before setting it as default store

--- src/CognitoSyncManager.js
+++ src/CognitoSyncManager.js
@@ -44,8 +44,26 @@ if (AWS === undefined) {
             };
         }
 
+        // Check for localStorage availability.
+        var isLsAvailable = (function() {
+            var uid = new Date();
+            var result;
+            try {
+                localStorage.setItem(uid, uid);
+                result = localStorage.getItem(uid) == uid;
+                localStorage.removeItem(uid);
+                return result;
+            } catch (exception) {
+                return false;
+            }
+        }());
+
         // Initialize local store.
-        this.local = new AWS.CognitoSyncManager.LocalStorage({DataStore: AWS.CognitoSyncManager.StoreLocalStorage});
+        var storageOps = {};
+        if (isLsAvailable) {
+            storageOps.DataStore = AWS.CognitoSyncManager.StoreLocalStorage;
+        }
+        this.local = new AWS.CognitoSyncManager.LocalStorage(storageOps);
 
         // Initialize remote store.
         this.remote = new AWS.CognitoSyncManager.RemoteStorage(this.identityPoolId, this.provider);
